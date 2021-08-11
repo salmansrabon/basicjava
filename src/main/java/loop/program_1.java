266ad2cc2fdc0ed418f6dbc4d551622d0e9bedf4
salmansrabon <kmsalman@springrainit.com>
2021-08-11 22:13:50 +0600

    update

--- src/main/java/loop/program_1.java
+++ src/main/java/loop/program_1.java
@@ -2,8 +2,12 @@ package loop;
 
 public class program_1 {
     public static void main(String[] args) {
+        //print even numbers
+        for(int i=0;i<=100;i+=2){
+            System.out.println(i);
+        }
+        //print odd numbers
         for(int i=1;i<=100;i+=2){
-
             System.out.println(i);
         }
     }
