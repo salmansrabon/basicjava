57fa59cc4ae2054e5df3b23db6c4924c687529d7
salmansrabon <kmsalman@springrainit.com>
2021-08-12 09:59:40 +0600

    update prime number code

--- src/main/java/loop/program_4.java
+++ src/main/java/loop/program_4.java
@@ -3,19 +3,19 @@ package loop;
 //check if a number is prime or not
 public class program_4 {
     public static void main(String[] args) {
-        int number=17;
+        int number=61;
         boolean flag=true;
         for(int i=2;i<=number/2;i++){
             if(number%i==0){
                 flag=false;
+                System.out.println("This is not prime");
                 break;
             }
         }
+
         if(flag==true){
             System.out.println("This is prime");
         }
-        else {
-            System.out.println("This is not prime");
-        }
+
     }
 }
